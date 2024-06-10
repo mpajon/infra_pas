ba6ff2e6eeee03e1550228a84fc6db63dc984fc4
Marcos Pajón <marcos.pajon@inetum.com>
2024-06-10 13:48:03 +0200

    Onboarding keycloak

--- imagenes_propias/onboarding/front/config/onboarding-env.js
+++ imagenes_propias/onboarding/front/config/onboarding-env.js
@@ -1,5 +1,8 @@
-(function (window) {
+  (function (window) {
     window.__env = window.__env || {};
     window.__env.api = "https://onboarding-back.asnw.inetum.com/api/";
+    window.__env.domain = "inetum.com";
+    window.__env.keycloak = "https://keycloak.asnw.inetum.com";
+    window.__env.realm = "onboarding";
+    window.__env.clientId = "onboarding";
   })(this);
-  
