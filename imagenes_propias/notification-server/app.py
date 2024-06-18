89ea9041dce8b3ba542116b84d5d4a243644d6ee
Marcos Pajón <marcos.pajon@inetum.com>
2024-06-18 14:38:47 +0200

    Notification-server temporal

--- imagenes_propias/notification-server/app.py
+++ imagenes_propias/notification-server/app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 import requests
 import os
 import logging
+import json
 
 app = Flask(__name__)
 
@@ -24,29 +25,51 @@ proxy = {
     "https": https_proxy,
 }
 
-@app.route('/api', methods=['POST'])
+@app.route('/kuma', methods=['POST'])
 def handle_post():
-    # Imprime información de la solicitud recibida
-    app.logger.debug(f"Recibida solicitud POST en '/api'")
-    app.logger.debug(f"Headers de la solicitud: {request.headers}")
-    app.logger.debug(f"Datos de la solicitud JSON: {request.json}")
-
     try:
-        url = "https://prod-211.westeurope.logic.azure.com:443/workflows/b167dbee4c5b446580aec8dbecfb3140/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=FxgJs3373YCmGl8o1hQkYfjwXrYQ4tmwrKpQBOJvs5k"
-        payload = {}
-        headers = {}
+        # Imprime información de la solicitud recibida
+        app.logger.debug(f"Recibida solicitud POST en '/api'")
+        app.logger.debug(f"Headers de la solicitud: {request.headers}")
+        app.logger.debug(f"Datos de la solicitud JSON: {request.json}")
+        
+        # Parsear el JSON
+        data = request.json
+        
+        # Verificar si hay errores o procesar los datos según sea necesario
+        if data:
+            sections = data.get('sections', [])
+            summary = data.get('summary')
+            text = None
+            for section in sections:        
+                if 'text' in section:
+                    text = section['text']
 
-        if proxy.get("http") and len(proxy["http"]) > 0:
-            response = requests.request("POST", url, headers=headers, data=payload, proxies=proxy)
+            # Retornar solo el 'summary' en la respuesta
+            response_data = {
+                'summary': summary,
+                'text': text
+            }               
+            url = "https://prod-211.westeurope.logic.azure.com:443/workflows/b167dbee4c5b446580aec8dbecfb3140/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=FxgJs3373YCmGl8o1hQkYfjwXrYQ4tmwrKpQBOJvs5k"
+            # Configurar los headers con Content-Type application/json
+            headers = {
+                'Content-Type': 'application/json'
+            }
+            payload = json.dumps(response_data)
+
+            if proxy.get("http") and len(proxy["http"]) > 0:
+                response = requests.request("POST", url, headers=headers, data=payload, proxies=proxy)
+            else:
+                response = requests.request("POST", url, headers=headers, data=payload)
+            
+            app.logger.debug(f"Response status code: {response.status_code}")
+            app.logger.debug(f"Response text: {response.text}")            
+            # Retorna una respuesta de ejemplo
+            response_data = {"mensaje": "Solicitud POST recibida correctamente"}
+            return jsonify(response_data), 200
         else:
-            response = requests.request("POST", url, headers=headers, data=payload)
+            return jsonify({'error': 'No se recibió JSON en la solicitud'}), 400           
 
-        app.logger.debug(f"Response status code: {response.status_code}")
-        app.logger.debug(f"Response text: {response.text}")
-
-        # Retorna una respuesta de ejemplo
-        response_data = {"mensaje": "Solicitud POST recibida correctamente"}
-        return jsonify(response_data), 200
     except requests.RequestException as e:
         # Maneja errores en la petición
         app.logger.error(f"Error en la solicitud: {str(e)}")
